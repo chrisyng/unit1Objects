6d37813bd2eb34e7e05254515aacc45cbdd02695
Christopher Ng <cyng@student.naperville203.org>
2014-09-07 21:51:53 -0500

    first change

---
+++ unit1Objects/TurtleLab/TurtleLab.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+public class TurtleLab
+{
+    public static void main (String[] args)
+    {
+        World pokemonWorld = new World();
+        Turtle torkoal = new Turtle(pokemonWorld);
+    }
+}
