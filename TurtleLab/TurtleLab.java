65ba68f0c9c66b6ec642ffa9e0f3cc9bc112ac8b
Christopher Ng <cyng@student.naperville203.org>
2014-09-08 19:32:52 -0500

    added turtles and worlds

--- TurtleLab/TurtleLab.java
+++ TurtleLab/TurtleLab.java
@@ -1,7 +1,25 @@
+import java.awt.Color;
 public class TurtleLab
 {
     public static void main(String args[])
+    throws InterruptedException
     {
-        System.out.println("Hello World!");
+        World pokemonWorld = new World(636, 503);
+        Turtle torkoal = new Turtle(pokemonWorld);        
+        Picture tallGrass = new Picture("tallgrass.png");
+        pokemonWorld.setPicture(tallGrass);
+        pokemonWorld.repaint();
+        torkoal.setColor(Color.RED);
+        torkoal.setShellColor(Color.GREEN);
+        torkoal.setPenColor(Color.BLUE);
+        torkoal.setPenWidth(8);
+        torkoal.forward(100);
+        Thread.sleep(1000);
+        torkoal.turnLeft();
+        Thread.sleep(500);
+        torkoal.forward(100);
+        Thread.sleep(200);
+        Picture battleScene = new Picture("battlescene.png");
+        
     }
 }
